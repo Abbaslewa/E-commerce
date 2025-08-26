f8cf42a61f829dd973ebdbc2e725ef5bc06f994b
AbbasLewa <abbasomerhassn@gmail.com>
2025-08-26 06:12:07 -0700

    new project

---
+++ src/components/ProductCrad.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { ProductType } from '@/types'
+import React from 'react'
+
+const ProductCrad = ({product}:{product:ProductType}) => {
+  return (
+    <div>ProductCrad</div>
+  )
+}
+
+export default ProductCrad
