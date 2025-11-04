d36f90a0fe549a5bb3ee1e1551155b3948d5cb9d
AbbasLewa <abbasomerhassn@gmail.com>
2025-11-04 17:43:36 +0300

    update

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,7 @@
 import ProductList from "@/components/ProductList";
 import Image from "next/image";
 
-const Homepage = () => {
+const Homep = () => {
   return (
     <div className="">
       <div className="relative aspect-[3/1] mb-12">
